c17fb0f7001c71c4c7a843b9f0ef897939f7b3f7
Md Mahruf Alam <baisakh2015@gmail.com>
2026-03-12 03:48:44 +0600

    server structure complete update 3

---
+++ server/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { DiseasesModule } from './modules/diseases/diseases.module';
+import { DoctorsModule } from './modules/doctors/doctors.module';
+import { MedicinesModule } from './modules/medicines/medicines.module';
+import { NutritionModule } from './modules/nutrition/nutrition.module';
+import { ReportsModule } from './modules/reports/reports.module';
+import { SearchModule } from './modules/search/search.module';
+import { UploadsModule } from './modules/uploads/uploads.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
+    AuthModule,
+    UsersModule,
+    DiseasesModule,
+    DoctorsModule,
+    MedicinesModule,
+    NutritionModule,
+    ReportsModule,
+    SearchModule,
+    UploadsModule,
+  ],
+})
+export class AppModule {}
